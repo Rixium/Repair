e7f1ff95c99bb795fb141e9bcbfe3579ce2bc390
Rixium <gamerpotty@gmail.com>
2020-01-31 19:00:16 +0000

    Set country with weather manager

--- Repair/Weather/WeatherManager.cs
+++ Repair/Weather/WeatherManager.cs
@@ -4,14 +4,22 @@ namespace Repair
     {
         private readonly string _apiKey;
 
+        private string _country = "UK";
+        private string _city = "Wrexham";
+
         public WeatherManager(string apiKey)
         {
             _apiKey = apiKey;
         }
 
-        public void SetCity()
+        public void SetCity(string city)
         {
-            
+            _city = city;
+        }
+
+        public void SetCountry(string country)
+        {
+            _country = country;
         }
         
     }
