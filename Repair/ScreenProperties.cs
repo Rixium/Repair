e6e00d68face72fa8b921403a8fb677c40df464f
Rixium <gamerpotty@gmail.com>
2020-01-31 21:09:56 +0000

    Add screen properties

---
+++ Repair/ScreenProperties.cs
@@ -0,0 +1,15 @@
+using Microsoft.Xna.Framework;
+
+namespace Repair
+{
+    public static class ScreenProperties
+    {
+
+        public static int ScreenWidth { get; set; }
+        public static int ScreenHeight { get; set; }
+        
+        public static Vector2 Center(int imageWidth, int imageHeight) => 
+            new Vector2(ScreenWidth / 2 - imageWidth / 2, ScreenHeight / 2 - imageHeight / 2);
+        
+    }
+}
