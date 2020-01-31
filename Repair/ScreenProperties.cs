5dcaa371ee580dc0abfe9312b9a3b1da981a55d3
Rixium <gamerpotty@gmail.com>
2020-01-31 22:06:56 +0000

    Add defaults to screen properties

--- Repair/ScreenProperties.cs
+++ Repair/ScreenProperties.cs
@@ -8,7 +8,7 @@ namespace Repair
         public static int ScreenWidth { get; set; }
         public static int ScreenHeight { get; set; }
         
-        public static Vector2 Center(int imageWidth, int imageHeight) => 
+        public static Vector2 Center(int imageWidth = 0, int imageHeight = 0) => 
             new Vector2(ScreenWidth / 2 - imageWidth / 2, ScreenHeight / 2 - imageHeight / 2);
         
     }
