26648fc4d437508ea0e005255854999657a981d0
Rixium <gamerpotty@gmail.com>
2020-01-31 18:36:48 +0000

    Add content chest

---
+++ Repair/ContentChest.cs
@@ -0,0 +1,21 @@
+using Microsoft.Xna.Framework.Content;
+
+namespace Repair
+{
+    public static class ContentChest
+    {
+        
+        private static ContentManager _contentManager;
+
+        public static void Initialize(ContentManager contentManager)
+        {
+            _contentManager = contentManager;
+        }
+        
+        public static void Load()
+        {
+            
+        }
+        
+    }
+}
